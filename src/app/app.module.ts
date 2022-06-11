7e033ff240bfbf51dc4b79f00bc4e6e4fe9441ad
Dmitry <dima.fenix@list.ru>
2022-06-11 13:32:08 +0300

    Add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { RegistrationPageComponent } from './auth/registration-page/registration-page.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ActiveComponent } from './active/active.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchComponent } from './search/search.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'active', component: ActiveComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', component: NotFoundComponent },
+  
+  
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegistrationPageComponent,
+    HomeComponent,
+    ProfileComponent,
+    ActiveComponent,
+    NotFoundComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
